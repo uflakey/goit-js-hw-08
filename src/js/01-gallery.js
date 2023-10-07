bc410de29886dfdd0e11d1c8d0fbbeab00ca8b7d
uflakey <134938478+uflakey@users.noreply.github.com>
2023-10-07 18:20:51 +0300

    111

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
+const modal = require('simpleLightbox');
+
 console.log(galleryItems);
+const containerGalary = document.querySelector('.gallery');
+
+containerGalary.insertAdjacentHTML('afterbegin', createMarkup(galleryItems));
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    )
+    .join('');
+}
+
+// const modal = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionDelay: 250,
+//   close: false,
+//   enableKeyboard: true,
+// });
