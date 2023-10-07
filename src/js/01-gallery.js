4c093d10fa1cadedc99d953f6ec1ab37e5998bae
uflakey <134938478+uflakey@users.noreply.github.com>
2023-10-07 22:00:31 +0300

    123

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Change code below this line
