ea06b999aaf955394338be9c67f7495116ef55b8
uflakey <134938478+uflakey@users.noreply.github.com>
2023-10-10 16:06:17 +0300

    0000

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,7 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+console.log(iframe);
+const player = new Vimeo.Player(iframe);
+console.log(player);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,9 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const button = document.querySelector('button');
+
+form.addEventListener(input, onInputForm);
+button.addEventListener(submit, onSubmitForm);
