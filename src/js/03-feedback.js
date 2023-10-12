a66f089494efdf669b8abc07a96dabc3058e006a
uflakey <134938478+uflakey@users.noreply.github.com>
2023-10-12 16:16:31 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,29 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('form');
-const input = document.querySelector('input');
-const textarea = document.querySelector('textarea');
-const button = document.querySelector('button');
+const form = document.querySelector('.feedback-form');
+console.log(form);
 
-form.addEventListener(input, onInputForm);
-button.addEventListener(submit, onSubmitForm);
+form.addEventListener('input', throttle(handlerInput, 500));
+form.addEventListener('submit', handlerSubmit);
 
+let data = {};
+console.log(data);
 
+function handlerInput(event) {
+  localStorage.setItem('feedback-form-state', data);
+}
 
+function handlerSubmit(event) {
+  event.preventDefault();
+  const { email, message } = event.currentTarget.elements;
+
+  data = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.getItem('feedback-form-state');
+  console.log(data);
+
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
